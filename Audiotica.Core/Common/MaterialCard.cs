d6ae609b2945dddc20573972be2fb99f5d603541
Harry <harry@zumicts.com>
2014-08-30 19:01:45 -0400

    Add MaterialCard content control
    prototype home page is using the new MaterialCard control.  Improve the
    template and made it into that content control.

---
+++ Audiotica.Core/Common/MaterialCard.cs
@@ -0,0 +1,18 @@
+﻿#region
+
+using Windows.UI.Xaml.Controls;
+
+#endregion
+
+// The Templated Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234235
+
+namespace Audiotica.Core.Common
+{
+    public sealed class MaterialCard : ContentControl
+    {
+        public MaterialCard()
+        {
+            DefaultStyleKey = typeof (MaterialCard);
+        }
+    }
+}
