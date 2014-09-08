8c7b5720756b0536a8b79f7895a0832ac6c01f4f
Harry <harry@zumicts.com>
2014-09-08 09:03:57 -0400

    Add mockup of artist page

---
+++ Apps/Audiotica.WindowsPhone/View/ArtistPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using Windows.UI.Xaml.Navigation;
+
+#endregion
+
+namespace Audiotica.View
+{
+    public sealed partial class ArtistPage
+    {
+        public ArtistPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
