45b98e544a12ea2cb233fcd5d2afc8c1515ae618
Harry <harry@zumicts.com>
2014-09-09 13:16:51 -0400

    Welcome message

--- Apps/Audiotica.WindowsPhone/View/HomePage.xaml.cs
+++ Apps/Audiotica.WindowsPhone/View/HomePage.xaml.cs
@@ -1,6 +1,11 @@
 ﻿#region
 
+using Windows.Storage;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 using Microsoft.Xbox.Music.Platform.Contract.DataModel;
 
@@ -15,6 +20,28 @@ namespace Audiotica.View
             InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var justUpdated = true;
+            var firstRun = !ApplicationData.Current.LocalSettings.Values.ContainsKey("CurrentBuild");
+
+            if (!firstRun)
+                justUpdated = (string) ApplicationData.Current.LocalSettings.Values["CurrentBuild"]
+                              != "1409-beta1-patch3";
+            else
+            {
+                ApplicationData.Current.LocalSettings.Values.Add("CurrentBuild", "1409-beta1-patch3");
+                new MessageDialog("This beta is meant for UI feedback and international testing of the charts api.", 
+                    "v1409 (BETA1) - Patch #3").ShowAsync();
+            }
+
+            if (!justUpdated || firstRun) return;
+            new MessageDialog("changelog here", "Patch #").ShowAsync();
+            ApplicationData.Current.LocalSettings.Values["CurrentBuild"] = "1409-beta1-patch3";
+        }
+
         //TODO [Harry,20140908] move this to view model with RelayCommand
         private void ListView_ItemClick(object sender, ItemClickEventArgs e)
         {
@@ -22,15 +49,15 @@ namespace Audiotica.View
             if (album != null) Frame.Navigate(typeof (AlbumPage), album.Id);
         }
 
-        private void SearchButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof (SearchPage));
         }
 
-        private void Grid_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
+        private void Grid_Tapped(object sender, TappedRoutedEventArgs e)
         {
             var artist = ((Grid) sender).DataContext as XboxArtist;
-            if (artist != null) Frame.Navigate(typeof(ArtistPage), artist.Id);
+            if (artist != null) Frame.Navigate(typeof (ArtistPage), artist.Id);
         }
     }
 }
