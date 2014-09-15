2a90f17889143eefcb2723bbe7d4fc14921ced1a
Harry <harry@zumicts.com>
2014-09-15 11:49:56 -0400

    Add interfaces for collection
    playlist is to be work on for patch #1

---
+++ Collections/Audiotica.Collection/ICollectionService.cs
@@ -0,0 +1,61 @@
+﻿#region License
+
+// Copyright (c) 2014 Harry
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+#region
+
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Audiotica.Collection.Model;
+
+#endregion
+
+namespace Audiotica.Collection
+{
+    internal interface ICollectionService
+    {
+        ObservableCollection<Song> Songs { get; set; }
+        ObservableCollection<Album> Albums { get; set; }
+        ObservableCollection<Artist> Artists { get; set; }
+
+        /// <summary>
+        ///     Loads all songs, albums, artist and playlists/queue.
+        /// </summary>
+        Task LoadLibraryAsync();
+
+        /// <summary>
+        ///     Adds the song to the database and collection.
+        /// </summary>
+        Task AddSongAsync(Song song, string artworkUrl);
+
+        /// <summary>
+        ///     Deletes the song from the database and collection.  Also all related files.
+        /// </summary>
+        Task DeleteSongAsync(Song song);
+
+        //TODO [Harry,20140915] everything related to the queue and playlists
+    }
+}
