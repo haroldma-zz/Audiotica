6b52aa825ce92d357403590c4d394670bb71be52
Harry <harry@zumicts.com>
2014-09-15 11:49:56 -0400

    Add interfaces for collection
    playlist is to be work on for patch #1

---
+++ Collections/Audiotica.Collection/IPlaylistService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audiotica.Collection
+{
+    interface IPlaylistService
+    {
+        //TODO [Harry,20140915] finish this for patch #1 of beta2
+    }
+}
---
+++ Collections/Audiotica.Collection/IQueueService.cs
@@ -0,0 +1,52 @@
+﻿#region License
+
+// Copyright (c) 2014 Harry
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+#endregion
+
+#region
+
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Audiotica.Collection.Model;
+
+#endregion
+
+namespace Audiotica.Collection
+{
+    internal interface IQueueService
+    {
+        ObservableCollection<QueueSong> PlaybackQueue { get; }
+
+        Task LoadQueueAsync();
+
+        Task ClearQueueAsync();
+
+
+        Task AddSongAsync(Song song);
+        Task MoveFromToAsync(int oldIndex, int newIndex);
+        Task DeleteAsync(QueueSong queueSongToRemove);
+
+    }
+}
