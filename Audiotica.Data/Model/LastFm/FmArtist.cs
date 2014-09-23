988d88cdb7cd0149112244522f852344b610f14c
Harry <harry@zumicts.com>
2014-09-23 15:41:03 -0400

    Add Last.Fm models

---
+++ Audiotica.Data/Model/LastFm/FmArtist.cs
@@ -0,0 +1,17 @@
+﻿#region
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace Audiotica.Data.Model.LastFm
+{
+    internal class FmArtist
+    {
+        public string name { get; set; }
+        public string listeners { get; set; }
+        public string mbid { get; set; }
+        public string url { get; set; }
+        public List<FmImage> image { get; set; }
+    }
+}
---
+++ Audiotica.Data/Model/LastFm/FmImage.cs
@@ -0,0 +1,16 @@
+﻿#region
+
+using Newtonsoft.Json;
+
+#endregion
+
+namespace Audiotica.Data.Model.LastFm
+{
+    public class FmImage
+    {
+        [JsonProperty(PropertyName = "#text")]
+        public string text { get; set; }
+
+        public string size { get; set; }
+    }
+}
