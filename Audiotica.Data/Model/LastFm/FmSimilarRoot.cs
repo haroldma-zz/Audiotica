352bdd22216738a04b86224ef3a9a20d52f07201
Harry <harry@zumicts.com>
2014-09-24 10:33:11 -0400

    Add last fm service
    missing music brainz

---
+++ Audiotica.Data/Model/LastFm/FmSimilarRoot.cs
@@ -0,0 +1,14 @@
+﻿#region
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace Audiotica.Data.Model.LastFm
+{
+    public class FmSimilarRoot
+    {
+        public FmArtistResults similarartists { get; set; }
+        public FmTrackResults similartracks { get; set; }
+    }
+}
---
+++ Audiotica.Data/Model/Musicbrainz/MbRelease.cs
@@ -0,0 +1,32 @@
+﻿#region
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+#endregion
+
+namespace Audiotica.Data.Model.Musicbrainz
+{
+    public class MbRelease
+    {
+        public string country { get; set; }
+        public string status { get; set; }
+        public string date { get; set; }
+        public object barcode { get; set; }
+
+        [JsonProperty(PropertyName = "release-events")]
+        public List<ReleaseEvent> releaseEvents { get; set; }
+
+        public string packaging { get; set; }
+        public string disambiguation { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public object asin { get; set; }
+        public string quality { get; set; }
+    }
+
+    public class ReleaseEvent
+    {
+        public string date { get; set; }
+    }
+}
