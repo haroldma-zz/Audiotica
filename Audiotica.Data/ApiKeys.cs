c85111c5e9b88b6ae7297b6686dd12da25c698f9
Harry <harry@zumicts.com>
2014-10-10 11:07:39 -0400

    Making ApiKeys.cs public
    since xbox music isn't being use anymore, no need to hide anything.  no
    secret key is being use for anything.

---
+++ Audiotica.Data/ApiKeys.cs
@@ -0,0 +1,20 @@
+﻿namespace Audiotica.Data
+{
+    public static class ApiKeys
+    {
+        public static string SoundCloudId
+        {
+            get { return "3204124e69a4dff75c8fc4a6b71174b7"; }
+        }
+        
+        public static string LastFmId
+        {
+            get { return "a2be27fc57d01bb6c2f982cf639df152"; }
+        }
+
+        public static string VkClientId
+        {
+            get { return "4413977"; }
+        }
+    }
+}
