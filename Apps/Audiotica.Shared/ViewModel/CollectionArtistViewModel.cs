daa97617895c4d70f2e2eadaf1d163320aa206c9
Harry <harry@zumicts.com>
2014-12-14 15:30:39 -0400

    Artist artwork in collection page
    also using songs instead of tracks

--- Apps/Audiotica.Shared/ViewModel/CollectionArtistViewModel.cs
+++ Apps/Audiotica.Shared/ViewModel/CollectionArtistViewModel.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using Windows.UI.Xaml.Controls;
 using Audiotica.Data.Collection;
 using Audiotica.Data.Collection.Model;
+using Audiotica.Data.Service.Interfaces;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight.Messaging;
+using IF.Lastfm.Core.Objects;
 
 #endregion
 
@@ -14,14 +16,17 @@ namespace Audiotica.ViewModel
 {
     public class CollectionArtistViewModel : ViewModelBase
     {
-        private readonly ICollectionService _service;
         private readonly AudioPlayerHelper _audioPlayer;
-        private Artist _artist;
+        private readonly ICollectionService _service;
+        private readonly IScrobblerService _lastService;
         private readonly RelayCommand<ItemClickEventArgs> _songClickCommand;
+        private Artist _artist;
+        private LastArtist _lastArtist;
 
-        public CollectionArtistViewModel(ICollectionService service, AudioPlayerHelper audioPlayer)
+        public CollectionArtistViewModel(ICollectionService service, IScrobblerService lastService, AudioPlayerHelper audioPlayer)
         {
             _service = service;
+            _lastService = lastService;
             _audioPlayer = audioPlayer;
             _songClickCommand = new RelayCommand<ItemClickEventArgs>(SongClickExecute);
             MessengerInstance.Register<GenericMessage<long>>(this, "artist-coll-detail-id", ReceivedId);
@@ -36,8 +41,20 @@ namespace Audiotica.ViewModel
             set { Set(ref _artist, value); }
         }
 
+        public LastArtist LastArtist
+        {
+            get { return _lastArtist; }
+            set { Set(ref _lastArtist, value); }
+        }
+
+        public RelayCommand<ItemClickEventArgs> SongClickRelayCommand
+        {
+            get { return _songClickCommand; }
+        }
+
         private void ReceivedId(GenericMessage<long> obj)
         {
+            LastArtist = null;
             SetArtist(obj.Content);
         }
 
@@ -57,14 +74,14 @@ namespace Audiotica.ViewModel
 #endif
         }
 
-        public RelayCommand<ItemClickEventArgs> SongClickRelayCommand
-        {
-            get { return _songClickCommand; }
-        }
-
-        private void SetArtist(long id)
+        private async void SetArtist(long id)
         {
             Artist = _service.Artists.FirstOrDefault(p => p.Id == id);
+            try
+            {
+                LastArtist = await _lastService.GetDetailArtist(Artist.Name);
+            }
+            catch { }
         }
     }
 }
