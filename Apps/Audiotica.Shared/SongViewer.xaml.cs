08f1bf7ee84801fb9baff21e2164474442041b73
Harry <harry@zumicts.com>
2015-01-14 13:51:50 -0400

    Use SongViewer for ItemTemplate in collections

---
+++ Apps/Audiotica.Shared/SongViewer.xaml.cs
@@ -0,0 +1,42 @@
+﻿#region
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+#endregion
+
+namespace Audiotica
+{
+    public sealed partial class SongViewer
+    {
+        public SongViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void AddToMenuFlyoutItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.Locator.Collection.Commands.AddToClickCommand.Execute(DataContext);
+        }
+
+        private void Picker_OnItemsPicked(ListPickerFlyout sender, ItemsPickedEventArgs args)
+        {
+            App.Locator.Collection.Commands.ItemPickedCommand.Execute(args.AddedItems[0]);
+        }
+
+        private void DeleteMenuFlyoutItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.Locator.Collection.Commands.DeleteClickCommand.Execute(DataContext);
+        }
+
+        private void DownloadButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.Locator.Collection.Commands.DownloadClickCommand.Execute(DataContext);
+        }
+
+        private void CancelButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            App.Locator.Collection.Commands.CancelClickCommand.Execute(DataContext);
+        }
+    }
+}
