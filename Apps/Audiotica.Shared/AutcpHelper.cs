87c6aa6204da13bddde108e527eb0e0b2d3e9737
zumicts <harry@zumicts.com>
2015-01-28 07:44:19 -0400

    Fix backup buffer bug

--- Apps/Audiotica.Shared/AutcpHelper.cs
+++ Apps/Audiotica.Shared/AutcpHelper.cs
@@ -8,9 +8,13 @@ using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Storage;
+
 using Audiotica.Core.Utils;
+
 using PCLStorage;
+
+using Windows.Storage;
+
 using CreationCollisionOption = PCLStorage.CreationCollisionOption;
 
 #endregion
@@ -20,50 +24,29 @@ namespace Audiotica
     public static class AutcpFormatHelper
     {
         public const int FormatVersion = 2;
+
         public const int FormatCompatabilityVersion = 2;
+
         private const int FileHeaderSize = 37;
 
-        public static async Task UnpackBackup(StorageFolder folder, Stream backupStream)
+        public static bool AddHeader(Stream stream)
         {
-            using (var zip = new ZipArchive(backupStream))
+            var fileHeader = new AutcpFileHeader
             {
-                foreach (var entry in zip.Entries)
-                {
-                    try
-                    {
-                        var name = entry.FullName;
+                signature = Encoding.UTF8.GetBytes("AUTCP"), 
+                version = FormatVersion, 
+                compatability = FormatCompatabilityVersion
+            };
 
-                        //compatability with Autcp v1
-                        if (name.Contains(".bksqldb"))
-                            name = name.Replace(".bksqldb", ".sqldb");
-
-                        var file =
-                            await
-                                StorageHelper.CreateFileAsync(name,
-                                    option: CreationCollisionOption.ReplaceExisting);
-                        using (var stream = await file.OpenAsync(FileAccess.ReadAndWrite))
-                        {
-                            using (var original = entry.Open())
-                            {
-                                await original.CopyToAsync(stream);
-                            }
-                        }
-                    }
-                    catch
-                    {
-                    }
-                }
-            }
+            return WriteFileHeader(stream, fileHeader);
         }
 
         public static async Task<byte[]> CreateBackup(StorageFolder folder)
         {
             using (var autcpStream = new MemoryStream())
             {
-                //add header now
+                // add header now
                 AddHeader(autcpStream);
-
-                #region Compress everything
 
                 using (var zipArchive = new ZipArchive(autcpStream, ZipArchiveMode.Create))
                 {
@@ -75,86 +58,52 @@ namespace Audiotica
 
                         if (item is StorageFile)
                         {
-                            var file = (item as StorageFile);
-                            if (file.FileType == ".autcp"
-                                || file.Name.ToLower().StartsWith("xam")
+                            var file = item as StorageFile;
+                            if (file.FileType == ".autcp" || file.Name.ToLower().StartsWith("xam")
                                 || file.Name.StartsWith("_"))
+                            {
                                 continue;
+                            }
+
                             files.Add(file);
                         }
                         else if (item is StorageFolder)
                         {
                             var name = (item as StorageFolder).Name;
-                            if (name == "SOMA" || name == "Logs" || name == "AdMediator"
-                                || name == "artists")
+                            if (name == "SOMA" || name == "Logs" || name == "AdMediator" || name == "artists")
+                            {
                                 continue;
+                            }
 
                             files.AddRange(await (item as StorageFolder).GetFilesAsync());
                         }
 
                         foreach (var file in files)
                         {
-                            var buffer = (await FileIO.ReadBufferAsync(file)).ToArray();
+                            var buffer = await FileIO.ReadBufferAsync(file);
 
-                            var path = file.Path.Replace(folder.Path + "\\", "").Replace("\\", "/");
+                            if (buffer.Length == 0)
+                            {
+                                continue;
+                            }
+
+                            var bytes = buffer.ToArray();
+
+                            var path = file.Path.Replace(folder.Path + "\\", string.Empty).Replace("\\", "/");
 
                             var entry = zipArchive.CreateEntry(path, CompressionLevel.Optimal);
                             using (var entryStream = entry.Open())
                             {
-                                await entryStream.WriteAsync(buffer, 0, buffer.Length);
+                                await entryStream.WriteAsync(bytes, 0, bytes.Length);
                                 await entryStream.FlushAsync();
                             }
                         }
                     }
                 }
 
-                #endregion
+                
 
                 return autcpStream.ToArray();
-            }
-        }
-
-        public static bool AddHeader(Stream stream)
-        {
-            var fileHeader = new AutcpFileHeader
-            {
-                signature = Encoding.UTF8.GetBytes("AUTCP"),
-                version = FormatVersion,
-                compatability = FormatCompatabilityVersion
-            };
-
-            return WriteFileHeader(stream, fileHeader);
-        }
-
-        public static bool ValidateHeader(Stream stream)
-        {
-            AutcpFileHeader header;
-            var valid = ReadFileHeader(stream, out header);
-            if (valid)
-            {
-                valid = FormatVersion >= header.compatability;
-            }
-            return valid;
-        }
-
-        public static bool WriteFileHeader(Stream stream, AutcpFileHeader fileHeader)
-        {
-            try
-            {
-                var buffer = new byte[FileHeaderSize];
-
-                var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
-                Marshal.StructureToPtr(fileHeader, gch.AddrOfPinnedObject(), false);
-                gch.Free();
-
-                stream.Seek(0, SeekOrigin.Begin);
-                stream.Write(buffer, 0, FileHeaderSize);
-
-                return true;
-            }
-            catch
-            {
-                return false;
             }
         }
 
@@ -184,13 +133,82 @@ namespace Audiotica
                 return false;
             }
         }
+
+        public static async Task UnpackBackup(StorageFolder folder, Stream backupStream)
+        {
+            using (var zip = new ZipArchive(backupStream))
+            {
+                foreach (var entry in zip.Entries)
+                {
+                    try
+                    {
+                        var name = entry.FullName;
+
+                        // compatability with Autcp v1
+                        if (name.Contains(".bksqldb"))
+                        {
+                            name = name.Replace(".bksqldb", ".sqldb");
+                        }
+
+                        var file =
+                            await StorageHelper.CreateFileAsync(name, option: CreationCollisionOption.ReplaceExisting);
+                        using (var stream = await file.OpenAsync(FileAccess.ReadAndWrite))
+                        {
+                            using (var original = entry.Open())
+                            {
+                                await original.CopyToAsync(stream);
+                            }
+                        }
+                    }
+                    catch
+                    {
+                    }
+                }
+            }
+        }
+
+        public static bool ValidateHeader(Stream stream)
+        {
+            AutcpFileHeader header;
+            var valid = ReadFileHeader(stream, out header);
+            if (valid)
+            {
+                valid = FormatVersion >= header.compatability;
+            }
+
+            return valid;
+        }
+
+        public static bool WriteFileHeader(Stream stream, AutcpFileHeader fileHeader)
+        {
+            try
+            {
+                var buffer = new byte[FileHeaderSize];
+
+                var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+                Marshal.StructureToPtr(fileHeader, gch.AddrOfPinnedObject(), false);
+                gch.Free();
+
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.Write(buffer, 0, FileHeaderSize);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1, Size = 37)]
     public struct AutcpFileHeader
     {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)] public byte[] signature;
-        public UInt16 version;
-        public UInt16 compatability;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 5)]
+        public byte[] signature;
+
+        public ushort version;
+
+        public ushort compatability;
     }
 }
