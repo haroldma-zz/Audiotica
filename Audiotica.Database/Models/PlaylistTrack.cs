9ca5d81e0182d16e984562f4d7a442c140a2e2db
Harry <harry@zumicts.com>
2015-08-05 21:01:44 -0400

    Commiting some non tracked files

---
+++ Audiotica.Database/Models/PlaylistTrack.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace Audiotica.Database.Models
+{
+    public class PlaylistTrack : DatabaseEntryBase
+    {
+        public long PrevId { get; set; }
+        public long TrackId { get; set; }
+        public long NextId { get; set; }
+
+        [Ignore]
+        public Track Track { get; set; }
+    }
+}
