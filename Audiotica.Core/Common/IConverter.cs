70e8e1b31fd0434bbbf8d4000a510268e69aa956
Harry <harry@zumicts.com>
2015-08-07 16:24:27 -0400

    Forgot to track -.-

---
+++ Audiotica.Core/Common/IConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Audiotica.Core.Common
+{
+    public interface IConverter<T, TT> where TT : IConvertibleObject
+    {
+        Task<T> ConvertAsync(TT other, Action<TT> saveChanges = null);
+    }
+
+    public interface IConvertibleObject
+    {
+        object PreviousConversion { get; set; }
+    }
+}
