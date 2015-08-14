c3799ee7ee4937dd446211cd1f2675bad5c7d201
Harry <harry@zumicts.com>
2015-08-14 10:14:11 -0400

    Creating some triggers and behaviours

---
+++ Windows/Audiotica.Windows/CustomTriggers/NetworkAvailableStateTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Windows.Networking.Connectivity;
+using Windows.UI.Xaml;
+
+namespace Audiotica.Windows.CustomTriggers
+{
+    public class NetworkAvailableStateTrigger : StateTriggerBase
+    {
+        public NetworkAvailableStateTrigger()
+        {
+            NetworkInformation.NetworkStatusChanged -= NetworkInformation_NetworkStatusChanged;
+            NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
+            UpdateState();
+        }
+
+        private async void NetworkInformation_NetworkStatusChanged(object sender)
+        {
+            await Dispatcher.RunAsync(global::Windows.UI.Core.CoreDispatcherPriority.Normal, UpdateState);
+        }
+
+        private void UpdateState()
+        {
+            var isConnected = false;
+            var profile = NetworkInformation.GetInternetConnectionProfile();
+            if (profile != null)
+                isConnected = profile.GetNetworkConnectivityLevel() == NetworkConnectivityLevel.InternetAccess;
+            SetActive(
+                 isConnected && ConnectionState.Equals(ConnectionStates.Connected) ||
+                !isConnected && ConnectionState.Equals(ConnectionStates.Disconnected));
+        }
+
+        private ConnectionStates _connectionState;
+        public ConnectionStates ConnectionState
+        {
+            get { return _connectionState; }
+            set
+            {
+                if (_connectionState == value)
+                    return;
+                _connectionState = value;
+                UpdateState();
+            }
+        }
+
+        public enum ConnectionStates
+        {
+            Connected,
+            Disconnected,
+        }
+    }
+}
---
+++ Windows/Audiotica.Windows/CustomTriggers/OrientationTrigger.cs
@@ -0,0 +1,83 @@
+﻿using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Audiotica.Core.Windows.Helpers;
+using Audiotica.Windows.Common;
+
+namespace Audiotica.Windows.CustomTriggers
+{
+    /// <summary>
+    ///     A trigger that changes state based on the orientation of the current window.
+    /// </summary>
+    public class OrientationTrigger : StateTriggerBase
+    {
+        #region Constructors
+
+        /// <summary>
+        ///     Initializes a new <see cref="OrientationTrigger" /> instance.
+        /// </summary>
+        public OrientationTrigger()
+        {
+            // Create a weak subscription to the SizeChanged event so that we don't pin the trigger or page in memory
+            WeakEvent.Subscribe<WindowSizeChangedEventHandler>(Window.Current, "SizeChanged", Window_SizeChanged);
+
+            // Calculate the initial state
+            CalculateState();
+        }
+
+        #endregion // Constructors
+
+        #region Internal Methods
+
+        private void CalculateState()
+        {
+            if (MobileOnly && !DeviceHelper.IsType(DeviceHelper.Family.Mobile))
+            {
+                WeakEvent.Unsubscribe<WindowSizeChangedEventHandler>(Window.Current, "SizeChanged", Window_SizeChanged);
+                return;
+            }
+
+            var currentOrientation = ApplicationView.GetForCurrentView().Orientation;
+            SetActive(currentOrientation == _orientation);
+        }
+
+        #endregion // Internal Methods
+
+        #region Overrides / Event Handlers
+
+        private void Window_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            // System.Diagnostics.Debug.WriteLine(string.Format("Size Changed {0}", this.GetHashCode()));
+            CalculateState();
+        }
+
+        #endregion // Overrides / Event Handlers
+
+        #region Public Properties
+
+        private ApplicationViewOrientation _orientation;
+
+        /// <summary>
+        ///     Gets or sets the orientation that will satisfy the trigger.
+        /// </summary>
+        /// <value>
+        ///     The orientation that will satisfy the trigger.
+        /// </value>
+        public ApplicationViewOrientation Orientation
+        {
+            get { return _orientation; }
+            set
+            {
+                if (_orientation != value)
+                {
+                    _orientation = value;
+                    CalculateState();
+                }
+            }
+        }
+
+        public bool MobileOnly { get; set; }
+
+        #endregion // Public Properties
+    }
+}
