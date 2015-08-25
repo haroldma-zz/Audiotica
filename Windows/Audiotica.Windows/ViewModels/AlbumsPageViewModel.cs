625fa1eb0cb8e385f4edcc76cd5796d2307d6d91
Harry <harry@zumicts.com>
2015-08-24 20:19:22 -0400

    Add grouping to albums page

--- Windows/Audiotica.Windows/ViewModels/AlbumsPageViewModel.cs
+++ Windows/Audiotica.Windows/ViewModels/AlbumsPageViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using Audiotica.Database.Services.Interfaces;
+using Audiotica.Windows.Services.Interfaces;
 using Audiotica.Windows.Tools.Mvvm;
 
 namespace Audiotica.Windows.ViewModels
 {
     public class AlbumsPageViewModel : ViewModelBase
     {
-        public AlbumsPageViewModel(ILibraryService libraryService)
+        public AlbumsPageViewModel(ILibraryService libraryService, ILibraryCollectionService libraryCollectionService)
         {
             LibraryService = libraryService;
+
+            AlbumsCollection = libraryCollectionService.AlbumsByTitle;
         }
 
         public ILibraryService LibraryService { get; }
+        public object AlbumsCollection { get; set; }
     }
 }
