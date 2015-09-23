3067f36d91eb399e3bf8afce2e4b104b47432696
Harold Martinez <hanthonym@outlook.com>
2015-09-23 18:17:05 -0400

    Bind playback position/duration to slider

--- Windows/Audiotica.Windows/ViewModels/PlayerBarViewModel.cs
+++ Windows/Audiotica.Windows/ViewModels/PlayerBarViewModel.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Linq;
-using Audiotica.Core.Utilities.Interfaces;
+using Windows.Media.Playback;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Audiotica.Database.Models;
 using Audiotica.Windows.Services.Interfaces;
 using Audiotica.Windows.Tools.Mvvm;
@@ -8,19 +11,25 @@ namespace Audiotica.Windows.ViewModels
 {
     public class PlayerBarViewModel : ViewModelBase
     {
-        private readonly IDispatcherUtility _dispatcherUtility;
         private readonly IPlayerService _playerService;
+        private readonly DispatcherTimer _timer;
         private QueueTrack _currentQueueTrack;
+        private double _playbackDuration;
+        private double _playbackPosition;
+        private Symbol _playPauseIcon = Symbol.Play;
 
-        public PlayerBarViewModel(IPlayerService playerService, IDispatcherUtility dispatcherUtility)
+        public PlayerBarViewModel(IPlayerService playerService)
         {
             _playerService = playerService;
-            _dispatcherUtility = dispatcherUtility;
             _playerService.TrackChanged += PlayerServiceOnTrackChanged;
+            _playerService.MediaStateChanged += PlayerServiceOnMediaStateChanged;
 
             PlayPauseCommand = new Command(() => _playerService.PlayOrPause());
             NextCommand = new Command(() => _playerService.Next());
             PrevCommand = new Command(() => _playerService.Previous());
+
+            _timer = new DispatcherTimer {Interval = TimeSpan.FromMilliseconds(500)};
+            _timer.Tick += TimerOnTick;
         }
 
         public Command PrevCommand { get; }
@@ -33,6 +42,46 @@ namespace Audiotica.Windows.ViewModels
         {
             get { return _currentQueueTrack; }
             set { Set(ref _currentQueueTrack, value); }
+        }
+
+        public Symbol PlayPauseIcon
+        {
+            get { return _playPauseIcon; }
+            set { Set(ref _playPauseIcon, value); }
+        }
+
+        public double PlaybackPosition
+        {
+            get { return _playbackPosition; }
+            set { Set(ref _playbackPosition, value); }
+        }
+
+        public double PlaybackDuration
+        {
+            get { return _playbackDuration; }
+            set { Set(ref _playbackDuration, value); }
+        }
+
+        private void TimerOnTick(object sender, object o)
+        {
+            PlaybackPosition = BackgroundMediaPlayer.Current.Position.TotalMilliseconds;
+            PlaybackDuration = BackgroundMediaPlayer.Current.NaturalDuration.TotalMilliseconds;
+        }
+
+        private void PlayerServiceOnMediaStateChanged(object sender, MediaPlayerState mediaPlayerState)
+        {
+            var icon = Symbol.Play;
+            switch (mediaPlayerState)
+            {
+                case MediaPlayerState.Playing:
+                    icon = Symbol.Pause;
+                    _timer.Start();
+                    break;
+                default:
+                    _timer.Stop();
+                    break;
+            }
+            PlayPauseIcon = icon;
         }
 
         private void PlayerServiceOnTrackChanged(object sender, string s)
