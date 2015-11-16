236974b83818356368e8434f91a5c88b35ee1eb7
Harold Martinez <hanthonym@outlook.com>
2015-11-16 11:42:45 -0400

    Fix Mp3Pm

--- Audiotica.Web/Http/Requets/MatchEngine/Mp3Truck/Mp3GluSearchRequest.cs
+++ Audiotica.Web/Http/Requets/MatchEngine/Mp3Truck/Mp3GluSearchRequest.cs
@@ -3,9 +3,9 @@ using HtmlAgilityPack;
 
 namespace Audiotica.Web.Http.Requets.MatchEngine.Mp3Truck
 {
-    public class Mp3TruckSearchRequest : RestObjectRequest<HtmlDocument>
+    public class Mp3GluSearchRequest : RestObjectRequest<HtmlDocument>
     {
-        public Mp3TruckSearchRequest(string query)
+        public Mp3GluSearchRequest(string query)
         {
             this.Url("http://mp3glu.com/search.php")
                 .QParam("q", query)
