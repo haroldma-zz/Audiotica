9c59c2ac966df7a89742787f4f98e446ad43792e
homroz <hmaciel@gmail.com>
2016-01-18 12:16:57 -0800

    Updating spotify chart request to use the new chart

--- Audiotica.Web/Http/Requets/Metadata/Spotify/Models/SpotifyChartsResponse.cs
+++ Audiotica.Web/Http/Requets/Metadata/Spotify/Models/SpotifyChartsResponse.cs
@@ -4,7 +4,22 @@ namespace Audiotica.Web.Http.Requets.Metadata.Spotify.Models
 {
     public class SpotifyChartsResponse
     {
-        public List<ChartTrack> Tracks { get; set; }
-        public string PrevDate { get; set; }
+        public string Country { get; set; }
+
+        public string Date { get; set; }
+
+        public string Description { get; set; }
+
+        public ChartEntry Entries { get; set; }
+
+        public string Href { get; set; }
+
+        public List<Image> Images { get; set; }
+
+        public string Name { get; set; }
+
+        public string Recurrence { get; set; }
+
+        public string Type { get; set; }
     }
 }
